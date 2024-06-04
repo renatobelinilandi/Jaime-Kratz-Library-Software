0e31f202b8f3a97a2ec647f178149696a76fd2ce
Renato Landi <128873438+renatobelinilandi@users.noreply.github.com>
2024-06-04 17:22:40 -0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,37 @@
+import pytz
+from datetime import datetime
+
+log_file = 'library_log.txt'
+admin_password = '123'
+
+def write_to_log(action):
+    action_ptbr = translate_action_to_portuguese(action)
+    br_tz = pytz.timezone('America/Sao_Paulo')
+    now = datetime.now(br_tz)
+    with open(log_file, 'a') as file:
+        file.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - {action_ptbr}\n")
+
+def translate_action_to_portuguese(action):
+    translations = {
+        "Updated quantity for existing book": "Quantidade atualizada para livro existente",
+        "Added new book": "Livro novo adicionado",
+        "Added units to book": "Unidades adicionadas ao livro",
+        "Removed one copy of book": "Uma cópia do livro removida",
+        "Removed complete book": "Livro removido completamente",
+        "Removed units from book": "Unidades removidas do livro",
+        "Rented book": "Livro alugado",
+        "Returned book": "Livro devolvido",
+        "Renewed loan for book": "Empréstimo do livro renovado",
+        "Admin password changed": "Senha do administrador alterada"
+    }
+    for key, value in translations.items():
+        if key in action:
+            return action.replace(key, value)
+    return action
+
+def center_window(window, window_width, window_height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = int((screen_width / 2) - (window_width / 2))
+    y = int((screen_height / 2) - (window_height / 2))
+    window.geometry(f"{window_width}x{window_height}+{x}+{y}")
